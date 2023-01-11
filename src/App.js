c1d67ba5ef9b1977001e691d292cd91c8f6e7833
Cristiano <crisbecker50@gmail.com>
2023-01-11 18:45:40 -0300

    requisito 2 pronto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
-    <Login />
+    <main>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={ Login }
+        />
+        <Route
+          exact
+          path="/game"
+          component={ Game }
+        />
+      </Switch>
+    </main>
   );
 }
