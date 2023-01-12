9f11864a97de50ceb77d378d23a90d12bf112ee8
icarols <icaro-live@hotmail.com>
2023-01-12 14:37:33 -0300

    main-group-19-req-5

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+
+class Header extends Component {
+  state = {
+    url: '',
+    score: 0,
+  };
+
+  componentDidMount() {
+    this.getProfilePicture();
+  }
+
+  getProfilePicture = () => {
+    const { email } = this.props;
+    const hash = md5(email).toString();
+    this.setState({ url: hash });
+  };
+
+  render() {
+    const { name } = this.props;
+    const { url, score } = this.state;
+    return (
+      <div>
+        <img
+          data-testid="header-profile-picture"
+          alt="profile"
+          src={ `https://www.gravatar.com/avatar/${url}` }
+        />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {}.isRequired;
+
+const mapStateToProps = (state) => ({
+  name: state.loginReducer.name,
+  email: state.loginReducer.email,
+});
+
+export default connect(mapStateToProps)(Header);
